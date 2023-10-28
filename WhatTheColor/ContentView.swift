7e0549df6fd93cda096d9e2edea99395605d21ae
Paweł Bijok <bijok.pawel@gmail.com>
2023-10-28 22:59:50 +0200

    Initial Commit

---
+++ WhatTheColor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhatTheColor
+//
+//  Created by Paweł Bijok on 28/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhatTheColor/WhatTheColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatTheColorApp.swift
+//  WhatTheColor
+//
+//  Created by Paweł Bijok on 28/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct WhatTheColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
