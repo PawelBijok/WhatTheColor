d40ee1727a01dcb2931aa79e3c9fa429d8163d65
Paweł Bijok <bijok.pawel@gmail.com>
2023-11-04 17:23:26 +0100

    Get pixel color at click

--- WhatTheColor/Helpers/MouseEventListener.swift
+++ WhatTheColor/Helpers/MouseEventListener.swift
@@ -11,6 +11,8 @@ import Cocoa
 class MouseEventListener {
     var eventMonitor: Any?
     
+    var lastMouseLocation : (Int, Int) = (0,0)
+    
     func startListening() {
         let mask: NSEvent.EventTypeMask = [.mouseMoved, .leftMouseDown]
         
@@ -18,11 +20,15 @@ class MouseEventListener {
         eventMonitor = NSEvent.addGlobalMonitorForEvents(matching: mask) { event in
             let eventType = event.type
             if(eventType == .leftMouseDown){
-                print("Thats a click")
+                let (x,y) = self.lastMouseLocation
+                print("Thats a click on x: \(x) y:\(y)")
+                self.getColorAtMouseLocation(mouseX: x, mouseY: y)
             }
             else if (eventType == .mouseMoved){
                 let mouseLocation = event.locationInWindow
                 print("Mouse moved to: \(mouseLocation)")
+                self.lastMouseLocation = (Int(mouseLocation.x), Int(mouseLocation.y))
+                
             }
         }
     }
@@ -31,6 +37,21 @@ class MouseEventListener {
         if let eventMonitor = eventMonitor {
             NSEvent.removeMonitor(eventMonitor)
             self.eventMonitor = nil
+        }
+    }
+    
+    func getColorAtMouseLocation(mouseX:Int, mouseY:Int) {
+        if let screen = NSScreen.main {
+            let frame = screen.frame
+                            let x = mouseX
+                            let y = Int(frame.height) - mouseY
+
+                            if let screenShot = CGDisplayCreateImage(CGMainDisplayID()) {
+                                let bitmapRep = NSBitmapImageRep(cgImage: screenShot)
+                                if x < bitmapRep.pixelsWide && y < bitmapRep.pixelsHigh {
+                                    print (bitmapRep.colorAt(x: x, y: y))
+                                }
+                            }
         }
     }
 }
