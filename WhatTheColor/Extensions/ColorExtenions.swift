49033440cf39ec5c72c05d2f95376ca63ad78d2b
Paweł Bijok <bijok.pawel@gmail.com>
2023-11-05 16:58:44 +0100

    add NSColor to hex String extension

--- WhatTheColor/Extensions/ColorExtenions.swift
+++ WhatTheColor/Extensions/ColorExtenions.swift
@@ -9,10 +9,17 @@ import Foundation
 import SwiftUI
 
 
-extension NSColor{
+extension NSColor {
     var asHexadecimal: String {
-        return "TODO"
+        let redHex = self.colorPartToHex(number: self.redComponent)
+        let greenHex = self.colorPartToHex(number: self.greenComponent)
+        let blueHex = self.colorPartToHex(number: self.blueComponent)
+        return "#\(redHex)\(greenHex)\(blueHex)"
     }
     
+    
+    private func colorPartToHex(number: CGFloat)-> String{
+        return String(Int(number * 255), radix: 16)
+    }
     
 }
